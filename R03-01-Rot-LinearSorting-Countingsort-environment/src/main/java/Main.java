1bb3b9477ccac1a879b8eb11b0eb8908d28e1968
lucasafo <lucasafo@localhost>
2018-05-08 09:32:34 -0300

    iae dg

--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/Main.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/Main.java
@@ -1,18 +1,18 @@
 import java.util.Arrays;
 
-import sorting.linearSorting.CountingSort;
+import sorting.linearSorting.ExtendedCountingSort;
 
 public class Main {
 
-	public static void main(String[] args) {
-		CountingSort count = new CountingSort();
-		
-		Integer[] array = {10,9,8,7,6,0,5,4,3,2,1};
-		
-		count.sort(array, 2, 9);
-		
-		System.out.println(Arrays.toString(array));
+   public static void main(String[] args) {
+      ExtendedCountingSort count = new ExtendedCountingSort();
 
-	}
+      Integer[] array = { 10, 9, 8, 7, -6, 35, 5, 4, 3, 2, 1 };
+
+      count.sort(array, 0, array.length - 1);
+
+      System.out.println(Arrays.toString(array));
+
+   }
 
 }
--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -10,10 +10,61 @@ import sorting.AbstractSorting;
  */
 public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
-	@Override
-	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   @Override
+   public void sort(Integer[] array, int leftIndex, int rightIndex) {
+      if (array != null && leftIndex < rightIndex && leftIndex >= 0 && rightIndex - leftIndex >= 1
+            && rightIndex < array.length) {
 
+         int dif = findMin(array, leftIndex, rightIndex);
+         Integer[] aux = new Integer[findMax(array, leftIndex, rightIndex) + 1 - (dif)];
+
+         for (int i = 0; i < aux.length; i++) {
+            aux[i] = 0;
+         }
+
+         for (int i = leftIndex; i <= rightIndex; i++) {
+            aux[array[i] - (dif)]++;
+         }
+
+         for (int i = 1; i < aux.length; i++) {
+            aux[i] = aux[i] + aux[i - 1];
+         }
+
+         int i = aux.length - 1, j = rightIndex;
+
+         while (i > 0) {
+            if (aux[i] > aux[i - 1]) {
+               array[j--] = i + (dif);
+               aux[i]--;
+            } else {
+               i--;
+            }
+         }
+
+         while (aux[0] > 0) {
+            array[j--] = 0 + (dif);
+            aux[0]--;
+         }
+      }
+   }
+
+   private int findMax(Integer[] array, int leftIndex, int rightIndex) {
+      int maxi = array[leftIndex];
+      for (int i = leftIndex; i <= rightIndex; i++) {
+         if (array[i] > maxi) {
+            maxi = array[i];
+         }
+      }
+      return maxi;
+   }
+
+   private int findMin(Integer[] array, int leftIndex, int rightIndex) {
+      int min = array[leftIndex];
+      for (int i = leftIndex; i <= rightIndex; i++) {
+         if (array[i] < min) {
+            min = array[i];
+         }
+      }
+      return min;
+   }
 }
