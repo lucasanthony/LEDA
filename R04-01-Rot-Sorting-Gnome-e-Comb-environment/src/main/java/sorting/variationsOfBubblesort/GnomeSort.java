98b0abdc856eec6b5ee1fbfa3dd31cddfb4fc557
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2018-05-10 14:43:54 -0300

    Update GnomeSort.java

--- R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/GnomeSort.java
+++ R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/GnomeSort.java
@@ -1,13 +1,28 @@
 package sorting.variationsOfBubblesort;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * The implementation of the algorithm must be in-place!
  */
 public class GnomeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		int pivot = leftIndex+1;
+		while(pivot<rightIndex) {
+			if (array[pivot].compareTo(array[pivot+1]) > 0) {
+				Util.swap(array, pivot, pivot+1);
+				if (pivot > leftIndex) {
+					pivot--;
+				}
+				
+				else {
+					pivot++;
+				}
+			}
+			else  {
+				pivot++;
+			}
+		}
 	}
 }
