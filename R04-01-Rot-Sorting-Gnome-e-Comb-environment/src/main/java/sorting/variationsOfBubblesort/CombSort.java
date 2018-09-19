614beb2de7ab9a0bdb8cbb6c4c46c61d3318416b
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2018-09-18 22:31:13 -0300

    Update CombSort.java
    Comb sort add

--- R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/CombSort.java
+++ R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/CombSort.java
@@ -8,7 +8,21 @@ import sorting.AbstractSorting;
 public class CombSort<T extends Comparable<T>> extends AbstractSorting<T> {
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		if (leftIndex < rightIndex && leftIndex >= 0 && rightIndex < array.length && array != null) {
+			int i = leftIndex;
+			int j = rightIndex - leftIndex +1;
+			int gap = (int) (j / 1.25);
+			while (gap > 0) {
+				if (i + gap > rightIndex) {
+					j--;
+					i = leftIndex;
+					gap = (int) (j / 1.25);
+				}
+				if (array[i].compareTo(array[i + gap]) > 0)
+					Util.swap(array, i, i + gap);
+
+				i++;
+			}
+		}
 	}
 }
